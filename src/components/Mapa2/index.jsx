a842c4c24172ed32d448b51ea5bd4cd09c98679f
ElvisRD <animeamv682@gmail.com>
2023-01-13 01:45:37 -0300

    solucion mapa

--- src/components/Mapa2/index.jsx
+++ src/components/Mapa2/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import MapView, { Marker, Callout } from 'react-native-maps';
+import MapView, { Marker, Callout, PROVIDER_DEFAULT, PROVIDER_GOOGLE } from 'react-native-maps';
 import styles from '../Mapa/styles';
 
 
 export default function Mapa2(){
+    const [mapProvider, setMapProvider] = useState(PROVIDER_DEFAULT);
+
 
     const initialRegion = {
         latitude: -34.603722,
@@ -16,6 +18,8 @@ export default function Mapa2(){
     return(
         <View style={styles.container}>
             <MapView
+                provider={mapProvider}
+                onMapReady={() => setMapProvider(PROVIDER_GOOGLE)}
                 initialRegion={initialRegion}  
                 style={styles.mapa}
             />
